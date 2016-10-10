4dc714517bc23e6e711f92553a8ed97006218274
LanKuDot <airlanser@gmail.com>
2016-10-11 02:27:51 +0800

    Add doxygen-style comment to the threadpool.c

--- threadpool.c
+++ threadpool.c
@@ -1,5 +1,9 @@
 #include "threadpool.h"
 
+/**
+ * @brief Release the memory allocated in _task\_t_
+ * @param the_task Pointer to the target _task\_t_
+ */
 int task_free(task_t *the_task)
 {
     free(the_task->arg);
@@ -7,8 +11,16 @@ int task_free(task_t *the_task)
     return 0;
 }
 
+/**
+ * @brief Initialize the members in _tqueue\_t_.
+ *
+ * Note that this function doesn't allocate memory for the task queue.
+ *
+ * @param the_queue Pointer to the target _tqueue\_t_
+ */
 int tqueue_init(tqueue_t *the_queue)
 {
+    // TODO: Check if the input pointer is vaild
     the_queue->head = NULL;
     the_queue->tail = NULL;
     pthread_mutex_init(&(the_queue->mutex), NULL);
@@ -17,10 +29,16 @@ int tqueue_init(tqueue_t *the_queue)
     return 0;
 }
 
+/**
+ * @brief Pop a task from the task queue.
+ * @param the_queue Pointer to the target task queue
+ * @return The popped task. NULL if the queue is empty.
+ */
 task_t *tqueue_pop(tqueue_t *the_queue)
 {
     task_t *ret;
     pthread_mutex_lock(&(the_queue->mutex));
+    // FIXME Pop the task from the head of the queue.
     ret = the_queue->tail;
     if (ret) {
         the_queue->tail = ret->last;
@@ -35,6 +53,11 @@ task_t *tqueue_pop(tqueue_t *the_queue)
     return ret;
 }
 
+/**
+ * @brief Get the number of the tasks in the task queue
+ * @param the_queue Pointer to the target task queue
+ * @return The number of the tasks in the task queue
+ */
 uint32_t tqueue_size(tqueue_t *the_queue)
 {
     uint32_t ret;
@@ -44,9 +67,15 @@ uint32_t tqueue_size(tqueue_t *the_queue)
     return ret;
 }
 
+/**
+ * @brief Push a task to the task queue
+ * @param the_queue Pointer to the target task queue
+ * @param task Pointer to the task to push
+ */
 int tqueue_push(tqueue_t *the_queue, task_t *task)
 {
     pthread_mutex_lock(&(the_queue->mutex));
+    // FIXME: Push the new task to the end of the queue.
     task->last = NULL;
     task->next = the_queue->head;
     if (the_queue->head)
@@ -58,11 +87,16 @@ int tqueue_push(tqueue_t *the_queue, task_t *task)
     return 0;
 }
 
+/**
+ * @brief Release the memory of the remaining tasks in the task queue and destory the mutex lock
+ * @param the_queue The target task queue
+ */
 int tqueue_free(tqueue_t *the_queue)
 {
     task_t *cur = the_queue->head;
     while (cur) {
         the_queue->head = the_queue->head->next;
+        // FIXME: Use task_free() instead.
         free(cur);
         cur = the_queue->head;
     }
@@ -70,6 +104,12 @@ int tqueue_free(tqueue_t *the_queue)
     return 0;
 }
 
+/**
+ * @brief Create and initialize the thread pool
+ * @param the_pool Pointer to the created thread pool
+ * @param tcount The number of the threads to be created
+ * @param func Pointer to the task function
+ */
 int tpool_init(tpool_t *the_pool, uint32_t tcount, void *(*func)(void *))
 {
     the_pool->threads = (pthread_t *) malloc(sizeof(pthread_t) * tcount);
@@ -85,6 +125,10 @@ int tpool_init(tpool_t *the_pool, uint32_t tcount, void *(*func)(void *))
     return 0;
 }
 
+/**
+ * @brief Join all the threads and release the allocated memory of thread pool
+ * @param the_pool The target thread pool
+ */
 int tpool_free(tpool_t *the_pool)
 {
     for (uint32_t i = 0; i < the_pool->count; ++i)
